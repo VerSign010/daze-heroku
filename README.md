0deaea0fff75aeb99eb1073059f16fb8964948f9
VerSign010 <54786470+VerSign010@users.noreply.github.com>
2020-08-30 07:33:41 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Daze Heroku
+
+# 用于在 Heroku 上部署 Daze。
+
+1、一键部署
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://dashboard.heroku.com/new?template=https%3A%2F%2Fgithub.com%2FVerSign010%2Fdaze-heroku)
+
+2、运行客户端
+daze client -l 127.0.0.1:1080 -s daze-herokuapp.com:443 -k daze123456 -dns 8.8.8.8:53
---
+++ configure.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Download and install Daze
+mkdir /tmp/daze
+curl -L -H "Cache-Control: no-cache" -o /tmp/daze/daze.zip https://github.com/mohanson/daze/releases/latest/download/daze_linux_amd64.zip
+unzip /tmp/daze/daze.zip -d /tmp/daze
+install -m 755 /tmp/daze/daze /daze
+
+# Remove temporary directory
+rm -rf /tmp/daze
+
+# Run Daze
+/daze server -l :$PORT -k $PASSWORD
